92737fb86b8986a1115b686e4505be9cedf4b720
exizzle-line-Shikura <naoto.shikura@exizzle-line.co.jp>
2022-10-20 18:23:05 +0900

    [modify] news一覧表示

--- _src/assets/js/parts/news.js
+++ _src/assets/js/parts/news.js
@@ -7,34 +7,12 @@ export default class News {
         request.onreadystatechange = () => {
             if (request.readyState == 4 && request.status == 200) {
                 const json = JSON.parse(request.responseText);
-                const news_top = document.getElementById('news_top')
-                const news_clm = document.getElementById('news_clm')
-                const en_news_top = document.getElementById('en_news_top')
-                const en_news_clm = document.getElementById('en_news_clm')
-                let NewsTop = '';
-                let NewsClm = '';
-                let EN_NewsTop = '';
-                let EN_NewsClm = '';
+                const news = document.getElementById('news')
+                const en_news = document.getElementById('en_news')
+                let News = '';
+                let EN_News = '';
                 for (let i = 0; i < json.length; i++) {
                     const NewsParts = 
-                    '<li class="page-news-list">' + 
-                        '<time class="page-news-time">' +
-                            '<span class="page-news-date">' +
-                                json[i].year + '&nbsp;' + json[i].date +
-                            '</span>' +
-                            '<span class="page-news-tag ' + 
-                                json[i].typetag +
-                            '">' +
-                                json[i].type +
-                            '</span>' +
-                        '</time>' +
-                        '<a href="' + json[i].link + '" class="page-news-link">' + 
-                            '<p class="page-news-text">' +
-                                json[i].text +
-                            '</p>' +
-                        '</a>' +
-                    '</li>';
-                    const NewsList = 
                     '<li class="page-news-list">' + 
                         '<time class="page-news-time">' +
                             '<span class="page-news-date">' +
@@ -53,25 +31,17 @@ export default class News {
                         '</a>' +
                     '</li>';
                 if (json[i].lang == "ja") {
-                    NewsTop += NewsParts;
-                    NewsClm += NewsList;
+                    News += NewsParts;
                 } 
                 else {
-                    EN_NewsTop += NewsParts;
-                    EN_NewsClm += NewsList;
+                    EN_News += NewsParts;
                 }
             }
-                if (news_top !== null){
-                    news_top.innerHTML = NewsTop;
+                if (news !== null){
+                    news.innerHTML = News;
                 }
-                if (news_clm !== null){
-                    news_clm.innerHTML = NewsClm;
-                }
-                if (en_news_top !== null){
-                    en_news_top.innerHTML = EN_NewsTop;
-                }
-                if (en_news_clm !== null){
-                    en_news_clm.innerHTML = EN_NewsClm;
+                if (en_news !== null){
+                    en_news.innerHTML = EN_News;
                 }
             }
         }
